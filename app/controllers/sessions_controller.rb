33389345611ba37925802399921700922b39cfb1
euqueme <euqueme@gmail.com>
2020-03-16 20:48:51 -0600

    Add strong params for followings and sessions controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(username: params[:session][:username])
+    user = User.find_by(session_params)
     if !user.nil?
       remember(user)
       flash[:info] = 'User Logged in'
@@ -22,4 +22,11 @@ class SessionsController < ApplicationController
     end
     redirect_to root_url
   end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:username)
+  end
+
 end
